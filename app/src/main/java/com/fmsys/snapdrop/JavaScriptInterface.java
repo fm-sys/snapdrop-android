837c2250507999588b610ce784222e0939da7117
Florian <64581222+fm-sys@users.noreply.github.com>
2020-10-06 11:03:13 +0200

    Initial commit

---
+++ app/src/main/java/com/fmsys/snapdrop/JavaScriptInterface.java
@@ -0,0 +1,171 @@
+package com.fmsys.snapdrop;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.MimeTypeMap;
+import android.widget.FrameLayout;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class JavaScriptInterface {
+    private MainActivity context;
+
+    public JavaScriptInterface(MainActivity context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data, String contentDisposition, String mimetype) throws IOException {
+        convertBase64StringToPdfAndStoreIt(base64Data, contentDisposition, mimetype);
+    }
+
+    public static String getBase64StringFromBlobUrl(String blobUrl, String mimetype) {
+        if (blobUrl.startsWith("blob")) {
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '" + blobUrl + "', true);" +
+                    "xhr.setRequestHeader('Content-type','" + mimetype + "');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobPdf = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobPdf);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            fileName = document.querySelector('a[href=\"" + blobUrl + "\"]').getAttribute('download');" +
+                    "            SnapdropAndroid.getBase64FromBlobData(base64data, fileName, \"" + mimetype + "\");" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf, String contentDisposition, String mimetype) throws IOException {
+        Log.e("BASE 64", base64PDf);
+        Log.e("Filename", contentDisposition);
+        final int notificationId = 1;
+
+        final File dwldsPath = getFinalNewDestinationFile(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS), contentDisposition);
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst("^data:" + mimetype + ";base64,", ""), 0);
+        if (dwldsPath.createNewFile()) {
+            FileOutputStream os = new FileOutputStream(dwldsPath, false);
+            os.write(pdfAsBytes);
+            os.flush();
+        }
+
+        if (dwldsPath.exists()) {
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            Uri apkURI = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider", dwldsPath);
+            intent.setDataAndType(apkURI, MimeTypeMap.getSingleton().getMimeTypeFromExtension(getFileExtension(contentDisposition)));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+            String CHANNEL_ID = "MYCHANNEL";
+            final NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "download notification", NotificationManager.IMPORTANCE_LOW);
+                Notification notification = new Notification.Builder(context, CHANNEL_ID)
+                        .setContentText(dwldsPath.getName())
+                        .setContentTitle("Download successful")
+                        .setContentIntent(pendingIntent)
+                        .setChannelId(CHANNEL_ID)
+                        .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                        .build();
+                if (notificationManager != null) {
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    notificationManager.notify(notificationId, notification);
+                }
+
+            } else {
+                NotificationCompat.Builder b = new NotificationCompat.Builder(context, CHANNEL_ID)
+                        .setDefaults(NotificationCompat.DEFAULT_ALL)
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                        //.setContentIntent(pendingIntent)
+                        .setContentTitle("MY TITLE")
+                        .setContentText("MY TEXT CONTENT");
+
+                if (notificationManager != null) {
+                    notificationManager.notify(notificationId, b.build());
+                    Handler h = new Handler();
+                    long delayInMilliseconds = 1000;
+                    h.postDelayed(() -> notificationManager.cancel(notificationId), delayInMilliseconds);
+                }
+            }
+
+            View coordinatorLayout = context.findViewById(R.id.coordinatorLayout);
+            Snackbar snackbar = Snackbar.make(coordinatorLayout, "file downloaded", Snackbar.LENGTH_SHORT);
+
+            final FrameLayout snackBarView = (FrameLayout) snackbar.getView();
+            snackBarView.setBackground(context.getResources().getDrawable(R.drawable.snackbar_larger_margin));
+            snackbar.show();
+            context.uploadIntent = null;
+        }
+    }
+
+    public static File getFinalNewDestinationFile(File destinationFolder, String filename) {
+
+        File newFile = new File(destinationFolder, filename);
+        if (!newFile.exists()) {
+            return newFile;
+        }
+
+        String nameWithoutExtensionOrIncrement;
+        String extension = getFileExtension(filename);
+
+        if (extension != null) {
+            extension = "." + extension;
+            int extInd = filename.lastIndexOf(extension);
+            nameWithoutExtensionOrIncrement = new StringBuilder(filename).replace(extInd, extInd + extension.length(), "").toString();
+        } else {
+            extension = "";
+            nameWithoutExtensionOrIncrement = filename;
+        }
+
+        int c = 0;
+        while (newFile.exists()) {
+            c++;
+            newFile = new File(destinationFolder, nameWithoutExtensionOrIncrement + " (" + c + ")" + extension);
+        }
+        return newFile;
+    }
+
+
+    public static String getFileExtension(String filename) {
+        if (filename == null) {
+            return null;
+        }
+        int lastUnixPos = filename.lastIndexOf('/');
+        int lastWindowsPos = filename.lastIndexOf('\\');
+        int indexOfLastSeparator = Math.max(lastUnixPos, lastWindowsPos);
+        int extensionPos = filename.lastIndexOf('.');
+        int indexOfExtension = indexOfLastSeparator > extensionPos ? -1 : extensionPos;
+        if (indexOfExtension == -1) {
+            return null;
+        } else {
+            return filename.substring(indexOfExtension + 1).toLowerCase();
+        }
+    }
+}
---
+++ app/src/main/java/com/fmsys/snapdrop/MainActivity.java
@@ -0,0 +1,200 @@
+package com.fmsys.snapdrop;
+
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends Activity {
+    private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 12321;
+    public WebView webView;
+    public SwipeRefreshLayout pullToRefresh;
+
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+
+    // workaround because snapdrop website do not show the correct devices after first load
+    private boolean loadAgain = true;
+
+    Intent uploadIntent = null;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        pullToRefresh = findViewById(R.id.pullToRefresh);
+
+        webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.addJavascriptInterface(new JavaScriptInterface(MainActivity.this), "SnapdropAndroid");
+        webView.setWebChromeClient(new MyWebChromeClient());
+        webView.clearCache(true);
+        webView.setWebViewClient(new WebViewClient() {
+
+            public void onPageFinished(WebView view, String url) {
+                if (loadAgain && url.equals("https://snapdrop.net/")) {
+                    loadAgain = false;
+                    refreshWebsite();
+                }
+            }
+        });
+
+        webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> webView.loadUrl(JavaScriptInterface.getBase64StringFromBlobUrl(url, mimetype)));
+
+        refreshWebsite();
+
+        Intent intent = getIntent();
+        if ((Intent.ACTION_SEND.equals(intent.getAction()) || Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction())) && intent.getType() != null) {
+            uploadIntent = intent;
+
+            View coordinatorLayout = findViewById(R.id.coordinatorLayout);
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, "A file is selected for sharing", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("drop", button -> uploadIntent = null)
+                    .setActionTextColor(getResources().getColor(R.color.colorAccent));
+
+
+            final FrameLayout snackBarView = (FrameLayout) snackbar.getView();
+            snackBarView.setBackground(getResources().getDrawable(R.drawable.snackbar_larger_margin));
+            snackbar.show();
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+            }
+        }
+
+        pullToRefresh.setOnRefreshListener(() -> {
+            refreshWebsite();
+            pullToRefresh.setRefreshing(false);
+        });
+
+    }
+
+    private void refreshWebsite() {
+        if (isInternetAvailable()) {
+            webView.loadUrl("https://snapdrop.net/");
+        } else {
+            String offlineHtml = "<html><body><div style=\"width: 100%; text-align:center; position: absolute; top: 20%; font-size: 30px;\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"500px\" height=\"500px\" fill=\"gray\" width=\"48px\" height=\"48px\"><path d=\"M0 0h24v24H0zm0 0h24v24H0z\" fill=\"none\"/><path d=\"M22 6V4H6.82l2 2H22zM1.92 1.65L.65 2.92l1.82 1.82C2.18 5.08 2 5.52 2 6v11H0v3h17.73l2.35 2.35 1.27-1.27L3.89 3.62 1.92 1.65zM4 6.27L14.73 17H4V6.27zM23 8h-6c-.55 0-1 .45-1 1v4.18l2 2V10h4v7h-2.18l3 3H23c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1z\"/></svg><br/><br/>Please verify that you are connected with a WiFi network</div></body></html>";
+            webView.loadData(offlineHtml, "text/html", "utf-8");
+        }
+    }
+
+    public boolean isInternetAvailable() {
+        final ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connMgr != null) {
+            NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+            if (activeNetworkInfo != null) {
+                if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (resultCode == RESULT_OK) {
+            uploadFromIntent(intent);
+        } else {
+            uploadMessage.onReceiveValue(null);
+            uploadMessage = null;
+        }
+
+
+    }
+
+    private void uploadFromIntent(Intent intent) {
+        Uri[] results = null;
+        try {
+            String dataString = intent.getDataString();
+            ClipData clipData = intent.getClipData();
+            if (clipData != null) {
+                results = new Uri[clipData.getItemCount()];
+                for (int i = 0; i < clipData.getItemCount(); i++) {
+                    ClipData.Item item = clipData.getItemAt(i);
+                    results[i] = item.getUri();
+                }
+            }
+            if (dataString != null) {
+                results = new Uri[]{Uri.parse(dataString)};
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "File chooser failed", Toast.LENGTH_SHORT).show();
+        }
+        uploadMessage.onReceiveValue(results);
+        uploadMessage = null;
+    }
+
+    class MyWebChromeClient extends WebChromeClient {
+
+        public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            if (uploadMessage != null) {
+                uploadMessage.onReceiveValue(null);
+                uploadMessage = null;
+            }
+
+            uploadMessage = filePathCallback;
+
+            if (uploadIntent != null) {
+                uploadFromIntent(uploadIntent);
+                return true;
+            }
+
+
+            Intent intent = fileChooserParams.createIntent();
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            try {
+                startActivityForResult(intent, REQUEST_SELECT_FILE);
+            } catch (ActivityNotFoundException e) {
+                uploadMessage = null;
+                Toast.makeText(MainActivity.this, "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+
+}
