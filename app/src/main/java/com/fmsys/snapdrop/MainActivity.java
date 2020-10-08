cb05e06b51f54f86b5c1e032f4cf2043fc3b5a23
Florian <64581222+fm-sys@users.noreply.github.com>
2020-10-08 14:12:21 +0200

    rework loading of website & app start

--- app/src/main/java/com/fmsys/snapdrop/MainActivity.java
+++ app/src/main/java/com/fmsys/snapdrop/MainActivity.java
@@ -4,15 +4,20 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.view.Window;
@@ -25,6 +30,7 @@ import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.core.content.ContextCompat;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -33,20 +39,41 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends Activity {
     private static final int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 12321;
     public WebView webView;
+    public LinearLayoutCompat imageViewLayout;
     public SwipeRefreshLayout pullToRefresh;
 
     public ValueCallback<Uri[]> uploadMessage;
     public static final int REQUEST_SELECT_FILE = 100;
 
-    // workaround because snapdrop website do not show the correct devices after first load
-    private boolean loadAgain = true;
+    private boolean loadAgain = true;     // workaround cause snapdrop website does not show the correct devices after first load
+    private boolean currentlyOffline = true;
+    private boolean currentlyLoading = false;
 
     public Intent uploadIntent = null;
+
+    private ConnectivityManager connMgr = null;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            if ((!isInitialStickyBroadcast()) && currentlyOffline) {
+                final Handler handler = new Handler();
+                handler.postDelayed(() -> {
+                    if (isInternetAvailable()) {
+                        refreshWebsite();
+                    }
+                }, 1500); // wait a bit until connection is ready
+
+            }
+        }
+    };
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
@@ -54,6 +81,8 @@ public class MainActivity extends Activity {
 
         webView = findViewById(R.id.webview);
         pullToRefresh = findViewById(R.id.pullToRefresh);
+        imageViewLayout = findViewById(R.id.splashImage);
+        connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
         webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
         webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
@@ -67,27 +96,19 @@ public class MainActivity extends Activity {
         webView.getSettings().setSupportMultipleWindows(true);
         webView.addJavascriptInterface(new JavaScriptInterface(MainActivity.this), "SnapdropAndroid");
         webView.setWebChromeClient(new MyWebChromeClient());
+        webView.setWebViewClient(new CustomWebViewClient());
         webView.clearCache(true);
-        webView.setWebViewClient(new WebViewClient() {
-
-            public void onPageFinished(WebView view, String url) {
-                if (loadAgain && url.equals("https://snapdrop.net/")) {
-                    loadAgain = false;
-                    refreshWebsite();
-                }
-            }
-        });
 
         webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> webView.loadUrl(JavaScriptInterface.getBase64StringFromBlobUrl(url, mimetype)));
 
         refreshWebsite();
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         if ((Intent.ACTION_SEND.equals(intent.getAction()) || Intent.ACTION_SEND_MULTIPLE.equals(intent.getAction())) && intent.getType() != null) {
             uploadIntent = intent;
 
-            View coordinatorLayout = findViewById(R.id.coordinatorLayout);
-            Snackbar snackbar = Snackbar
+            final View coordinatorLayout = findViewById(R.id.coordinatorLayout);
+            final Snackbar snackbar = Snackbar
                     .make(coordinatorLayout, "A file is selected for sharing", Snackbar.LENGTH_INDEFINITE)
                     .setAction("drop", button -> uploadIntent = null)
                     .setActionTextColor(getResources().getColor(R.color.colorAccent));
@@ -108,28 +129,32 @@ public class MainActivity extends Activity {
             pullToRefresh.setRefreshing(false);
         });
 
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(receiver, intentFilter);
+
     }
 
     private void refreshWebsite() {
         if (isInternetAvailable()) {
             webView.loadUrl("https://snapdrop.net/");
         } else {
-            String offlineHtml = "<html><body><div style=\"width: 100%; text-align:center; position: absolute; top: 20%; font-size: 30px;\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"500px\" height=\"500px\" fill=\"gray\" width=\"48px\" height=\"48px\"><path d=\"M0 0h24v24H0zm0 0h24v24H0z\" fill=\"none\"/><path d=\"M22 6V4H6.82l2 2H22zM1.92 1.65L.65 2.92l1.82 1.82C2.18 5.08 2 5.52 2 6v11H0v3h17.73l2.35 2.35 1.27-1.27L3.89 3.62 1.92 1.65zM4 6.27L14.73 17H4V6.27zM23 8h-6c-.55 0-1 .45-1 1v4.18l2 2V10h4v7h-2.18l3 3H23c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1z\"/></svg><br/><br/>Please verify that you are connected with a WiFi network</div></body></html>";
+            final String offlineHtml = "<html><body><div style=\"width: 100%; text-align:center; position: absolute; top: 20%; font-size: 30px;\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"500px\" height=\"500px\" fill=\"gray\" width=\"48px\" height=\"48px\"><path d=\"M0 0h24v24H0zm0 0h24v24H0z\" fill=\"none\"/><path d=\"M22 6V4H6.82l2 2H22zM1.92 1.65L.65 2.92l1.82 1.82C2.18 5.08 2 5.52 2 6v11H0v3h17.73l2.35 2.35 1.27-1.27L3.89 3.62 1.92 1.65zM4 6.27L14.73 17H4V6.27zM23 8h-6c-.55 0-1 .45-1 1v4.18l2 2V10h4v7h-2.18l3 3H23c.55 0 1-.45 1-1V9c0-.55-.45-1-1-1z\"/></svg><br/><br/>Please verify that you are connected with a WiFi network</div></body></html>";
             webView.loadData(offlineHtml, "text/html", "utf-8");
+            currentlyOffline = true;
         }
     }
 
     public boolean isInternetAvailable() {
-        final ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connMgr != null) {
-            NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+            final NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
             return activeNetworkInfo != null && activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI;
         }
         return false;
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
 
         if (resultCode == RESULT_OK) {
@@ -139,7 +164,12 @@ public class MainActivity extends Activity {
             uploadMessage = null;
         }
 
+    }
 
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(receiver);
+        super.onDestroy();
     }
 
     private void uploadFromIntent(final Intent intent) {
@@ -201,6 +231,50 @@ public class MainActivity extends Activity {
             return false;
         }
 
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageFinished(final WebView view, final String url) {
+            currentlyLoading = false;
+
+            if (url.startsWith("https://snapdrop.net/")) {
+                currentlyOffline = false;
+
+                if (loadAgain) {
+                    loadAgain = false;
+                    refreshWebsite();
+                }
+            }
+
+            imageViewLayout.setVisibility(View.GONE);
+            pullToRefresh.setVisibility(View.VISIBLE);
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onPageStarted(final WebView view, final String url, final Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+            final Handler handler = new Handler();
+            final int delay = 500; // milliseconds
+            currentlyLoading = true;
+
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    //do something
+                    if (currentlyLoading) {
+                        if (isInternetAvailable()) {
+                            handler.postDelayed(this, delay);
+                        } else {
+                            refreshWebsite();
+                        }
+                    }
+                }
+            }, delay);
+
+        }
 
     }
 
