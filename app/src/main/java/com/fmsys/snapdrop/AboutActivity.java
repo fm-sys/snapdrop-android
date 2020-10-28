9a5b540fb80d4c954b5c617459127d72369161a9
Florian <64581222+fm-sys@users.noreply.github.com>
2020-10-28 21:23:39 +0100

    add about activity (currently only accessible via long click on launcher icon - available since Android 7)

---
+++ app/src/main/java/com/fmsys/snapdrop/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.fmsys.snapdrop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutActivity extends Activity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        final TextView version = findViewById(R.id.version);
+        version.setText("v" + BuildConfig.VERSION_NAME);
+    }
+}
