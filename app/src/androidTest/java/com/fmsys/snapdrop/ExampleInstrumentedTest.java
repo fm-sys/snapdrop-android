dbb57f94c459e0e0d42581db2388aeca8c51d448
Florian <64581222+fm-sys@users.noreply.github.com>
2021-01-12 22:09:50 +0100

    fix codacy issues and add some comments about how unit tests are executed from our CI

--- app/src/androidTest/java/com/fmsys/snapdrop/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fmsys/snapdrop/ExampleInstrumentedTest.java
@@ -2,12 +2,11 @@ package com.fmsys.snapdrop;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +16,33 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    /**
+
+    // InstrumentedTests are currently not executed in our CI
+    // If it should be necessary in the future, following GitHub Actions workflow might help
+
+jobs:
+  test:
+    runs-on: macOS-latest
+    steps:
+    - name: checkout
+      uses: actions/checkout@v1
+      with:
+        fetch-depth: 1
+
+    - name: run tests
+      uses: reactivecircus/android-emulator-runner@v1
+      with:
+        api-level: 29
+        script: ./gradlew connectedCheck
+    */
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        final Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.snapdrop", appContext.getPackageName());
     }
 }
