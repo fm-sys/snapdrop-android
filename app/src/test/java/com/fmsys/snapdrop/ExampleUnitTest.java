73408852209f0b900624a4c1b899311e4f996771
Florian <64581222+fm-sys@users.noreply.github.com>
2021-01-12 20:42:11 +0100

    fix some codacy issues

--- app/src/test/java/com/fmsys/snapdrop/ExampleUnitTest.java
+++ app/src/test/java/com/fmsys/snapdrop/ExampleUnitTest.java
@@ -1,11 +1,12 @@
 package com.fmsys.snapdrop;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
+ *
+ * These tests are also integrated in our build CI
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
