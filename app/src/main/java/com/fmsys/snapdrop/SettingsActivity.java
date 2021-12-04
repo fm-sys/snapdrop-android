8dfefe5e16cc716d53ae54bf27a3e2d2afb24c9b
fm-sys <64581222+fm-sys@users.noreply.github.com>
2021-12-04 15:41:40 +0100

    add Actionbar to SettingsActivity

--- app/src/main/java/com/fmsys/snapdrop/SettingsActivity.java
+++ app/src/main/java/com/fmsys/snapdrop/SettingsActivity.java
@@ -2,8 +2,11 @@ package com.fmsys.snapdrop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,11 +15,25 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         SnapdropApplication.setAppTheme(this);
 
+        setContentView(R.layout.activity_settings);
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .replace(R.id.settings_fragment_root, new SettingsFragment())
                 .commit();
 
         setResult(Activity.RESULT_OK);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull final MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
