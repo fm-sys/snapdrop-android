ff11f3b69a08bece237bdb1ab6ea7a018e36b843
fm-sys <64581222+fm-sys@users.noreply.github.com>
2022-02-27 01:11:11 +0100

    Add quick settings tile (fix #177)

---
+++ app/src/main/java/com/fmsys/snapdrop/QuickTileService.java
@@ -0,0 +1,36 @@
+package com.fmsys.snapdrop;
+
+import android.content.Intent;
+import android.os.Build;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+public class QuickTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+
+        final Tile tile = getQsTile();
+        tile.setState(Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        if (isLocked()) {
+            unlockAndRun(this::startSnapdrop);
+        } else {
+            startSnapdrop();
+        }
+    }
+
+    private void startSnapdrop() {
+        startActivityAndCollapse(new Intent(this, MainActivity.class).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+}
